5454cff75ef640c3ddf1f0a34fc4b4b57e43d3c7
60152180 <donkey3353@naver.com>
2016-12-14 00:01:16 +0900

    fix app.js

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var methodOverride = require('method-override');
 var flash = require('connect-flash');
+var mongoose   = require('mongoose');
 var passport = require('passport');
 var configAuth = require('./config/auth');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -15,12 +18,20 @@ var posts = require('./routes/posts');
 var auth = require('./routes/auth');
 
 var mongoose   = require('mongoose');
+var routeAuth = require('./routes/auth');
+var app = express();
+
+var routeAuth = require('./routes/auth');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
+app.locals.moment = require('moment');
 
 // 몽고DB 연결
 mongoose.connect('mongodb://hyein:123123@ds139847.mlab.com:39847/coconut');
@@ -32,7 +43,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
+
 
 app.use(session({
   resave: true,
@@ -62,6 +74,9 @@ app.use('/users', users);
 app.use('/posts', posts);
 auth(app, passport);
 
+//app.use('/hosting', hosting);
+routeAuth(app, passport);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -69,15 +84,29 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
